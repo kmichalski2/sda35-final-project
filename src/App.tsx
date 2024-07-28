ebde0fbbc23ceb958ebb3aea29a3bea5d0903eee
Kamil Michalski <kmichalski@outlook.com>
2024-07-28 13:18:05 +0200

    list employees

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,51 @@
 import './App.css'
+import { Table } from './components/Table'
+
+export interface Employee {
+  id: string;
+  firstname: string;
+  lastname: string;
+  salary: number;
+  status: string;
+}
 
 function App() {
+  const mockData: Employee[] = [
+    {
+      id: '1',
+      firstname: 'John',
+      lastname: 'Doe',
+      salary: 5000,
+      status: 'Na urlopie'
+    },
+    {
+      id: '2',
+      firstname: 'Jan',
+      lastname: 'Kowalskiego',
+      salary: 15000,
+      status: 'Na urlopie'
+    },
+    {
+      id: '3',
+      firstname: 'Adam',
+      lastname: 'Nowak',
+      salary: 25000,
+      status: 'Na urlopie'
+    },
+    {
+      id: '3',
+      firstname: 'Adam',
+      lastname: 'Nowak',
+      salary: 25000,
+      status: 'Na urlopie'
+    },
+  ]
+
   return (
     <>
       <h1>Employees</h1>
+
+      <Table data={mockData}></Table>
     </>
   )
 }
---
+++ src/components/Table.tsx
@@ -0,0 +1,30 @@
+import { Employee } from '../App';
+
+export interface TableProps {
+    data: Employee[];
+}
+
+export function Table({data}: TableProps) {
+    return (
+        <table>
+            <thead>
+            <tr>
+                <th>ID</th>
+                <th>Firstname</th>
+                <th>Lastname</th>
+                <th>Salary</th>
+                <th>Status</th>
+            </tr>
+            </thead>
+            <tbody>
+            { data.map(item => <tr>
+                <td>{item.id}</td>
+                <td>{item.firstname}</td>
+                <td>{item.lastname}</td>
+                <td>{item.salary}</td>
+                <td>{item.status}</td>
+            </tr>)}
+            </tbody>
+      </table>
+    )
+}
